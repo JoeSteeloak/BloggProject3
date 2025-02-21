74e040d379b147c99ca0276f564ef968013dacd0
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-02-21 10:32:24 +0100

    add login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,50 @@
+import { useState } from "react"
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, seterror] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        seterror('');
+    };
+
     return (
-        <div>LoginPage</div>
+        <>
+        <h2>Logga in</h2>
+        <form onSubmit={handleSubmit}>
+            {error && (
+                <div className="error-message">
+                    {error}
+                </div>
+            )} 
+
+            <div>
+                <label htmlFor="email">E-postadress</label>
+                <input 
+                id="email" 
+                type="email" 
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+
+            <div>
+                <label htmlFor="password">Lösenord</label>
+                <input 
+                id="password" 
+                type="password" 
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+
+            <button type="submit">Logga in</button>
+
+        </form>
+        </>
+        
     )
 }
 
